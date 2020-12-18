18a53d86df76a712bf60b73e9660bd1bee1a14e6
juantc93 <52570565+juantc93@users.noreply.github.com>
2020-12-18 00:12:49 -0500

    nuevo commit

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI,HTTPException
 import db
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
+origins=[
+    "http//localhost.tiangolo.com","https://localhost.tiangolo.com",
+    "http//localhost","http://localhost:8080","https://prueba-frontend-hotel-tic.herokuapp.com",
+]
 
+app.add_middleware(
+    CORSMiddleware,allow_origins=origins,
+    allow_credentials=True, allow_methods=["*"],allow_headers=["*"],
+)
 @app.get("/reservas/ocupacion")
 async def obtener_ocupacion():
     return db.obtener_lista_reservas()
